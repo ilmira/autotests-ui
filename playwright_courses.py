854945b8d3a6d8caa8fc55db8b8d634a6144b2ee
ilmira <ilmiram625@gmail.com>
2026-02-23 15:57:48 +0300

    Добавлен файл playwright_courses.py

--- playwright_courses.py
+++ playwright_courses.py
@@ -29,23 +29,36 @@ with sync_playwright() as playwright:
     registration_button.click()
 
     # Сохраняем состояние браузера (куки и localStorage) в файл для дальнейшего использования
-    context.storage_state(path="browser-state.json")
-
-    # Проверяем, что появилось сообщение на странице Dashboard
-    dashboard_title = page.get_by_test_id('dashboard-toolbar-title-text')
-    expect(dashboard_title).to_be_visible()
-    expect(dashboard_title).to_have_text('Dashboard')
-
-    # Задержка для наглядности выполнения теста (не рекомендуется использовать в реальных автотестах)
-    page.wait_for_timeout(5000)
+    context.storage_state(path="browser-state_for_courses.json")
 
 # Использование сохраненного контекста браузера, для попадания сразу на страницу Dashboard
 
 with sync_playwright() as playwright:
     browser = playwright.chromium.launch(headless=False)
-    context = browser.new_context(storage_state="browser-state.json") # Указываем файл с сохраненным состоянием
+    context = browser.new_context(
+        storage_state="browser-state_for_courses.json")  # Указываем файл с сохраненным состоянием
     page = context.new_page()
 
-    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/dashboard")
+    page.goto(" https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
+
+    # Проверяем наличие и текст заголовка "Courses"
+    courses_title = page.get_by_test_id('courses-list-toolbar-title-text')
+    expect(courses_title).to_be_attached()
+    expect(courses_title).to_have_text('Courses')
+
+    # Проверяем наличие и текст блока "There is no results"
+    no_results_text = page.get_by_test_id('courses-list-empty-view-title-text')
+    expect(no_results_text).to_be_attached()
+    expect(no_results_text).to_have_text('There is no results')
+
+    # Проверяем наличие и видимость иконки пустого блока
+    icon = page.get_by_test_id('courses-list-empty-view-icon')
+    expect(icon).to_be_attached()
+    expect(icon).to_be_visible()
+
+    # Проверяем наличие и текст описания блока: "Results from the load test pipeline will be displayed here"
+    result_displayed_text = page.get_by_test_id('courses-list-empty-view-description-text')
+    expect(result_displayed_text).to_be_attached()
+    expect(result_displayed_text).to_have_text('Results from the load test pipeline will be displayed here')
 
     page.wait_for_timeout(5000)
