0458c5589edfaa31a000c533971401213cf3af6a
ilmira <ilmiram625@gmail.com>
2026-02-24 23:55:38 +0300

    Частично реализован POM 2.0

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -1,28 +1,46 @@
 import pytest
 from playwright.sync_api import expect
 
+from pages.courses_list_page import CoursesListPage
+from pages.create_course_page import CreateCoursePage
+
 
 @pytest.mark.courses
 @pytest.mark.regression
-def test_empty_courses_list(chromium_page_with_state):
-    chromium_page_with_state.goto(" https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
+def test_empty_courses_list(courses_list_page: CoursesListPage):
+    courses_list_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
+    courses_list_page.check_visible_courses_title()
+    courses_list_page.check_visible_empty_view()
 
-    # Проверяем наличие и текст заголовка "Courses"
-    courses_title = chromium_page_with_state.get_by_test_id('courses-list-toolbar-title-text')
-    expect(courses_title).to_be_attached()
-    expect(courses_title).to_have_text('Courses')
 
-    # Проверяем наличие и текст блока "There is no results"
-    no_results_text = chromium_page_with_state.get_by_test_id('courses-list-empty-view-title-text')
-    expect(no_results_text).to_be_attached()
-    expect(no_results_text).to_have_text('There is no results')
+@pytest.mark.courses
+@pytest.mark.regression
+def test_create_course(create_course_page: CreateCoursePage, courses_list_page: CoursesListPage):
+    create_course_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create")
 
-    # Проверяем наличие и видимость иконки пустого блока
-    icon = chromium_page_with_state.get_by_test_id('courses-list-empty-view-icon')
-    expect(icon).to_be_attached()
-    expect(icon).to_be_visible()
+    create_course_page.check_visible_create_course_title()
+    create_course_page.check_disabled_create_course_button()
+    create_course_page.check_visible_image_preview_empty_view()
+    create_course_page.check_visible_image_upload_view()
 
-    # Проверяем наличие и текст описания блока: "Results from the load test pipeline will be displayed here"
-    result_displayed_text = chromium_page_with_state.get_by_test_id('courses-list-empty-view-description-text')
-    expect(result_displayed_text).to_be_attached()
-    expect(result_displayed_text).to_have_text('Results from the load test pipeline will be displayed here')
+    create_course_page.check_visible_create_course_form('', '', '', '0', '0')
+    create_course_page.check_visible_exercises_title()
+    create_course_page.check_visible_create_exercise_button()
+
+    create_course_page.upload_preview_image('./testdata/files/image.png')
+    create_course_page.check_visible_image_upload_view()
+
+    create_course_page.fill_create_course_form(title="Playwright",
+                                               estimated_time="2 weeks",
+                                               description="Playwright",
+                                               max_score="100",
+                                               min_score="10")
+    create_course_page.click_create_course_button()
+
+    courses_list_page.check_visible_courses_title()
+    courses_list_page.check_visible_create_course_button()
+    courses_list_page.check_visible_course_card(index=0,
+                                                title="Playwright",
+                                                estimated_time="2 weeks",
+                                                max_score="100",
+                                                min_score="10")
