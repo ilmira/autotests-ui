242aefda232c21e76be50d74dfbeb02f3321fd79
ilmira <ilmiram625@gmail.com>
2026-02-25 18:49:04 +0300

    Частично добавлен Page Component

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -1,6 +1,4 @@
 import pytest
-from playwright.sync_api import expect
-
 from pages.courses_list_page import CoursesListPage
 from pages.create_course_page import CreateCoursePage
 
@@ -9,6 +7,8 @@ from pages.create_course_page import CreateCoursePage
 @pytest.mark.regression
 def test_empty_courses_list(courses_list_page: CoursesListPage):
     courses_list_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
+    courses_list_page.navbar.check_visible('username')
+    courses_list_page.sidebar.check_visible()
     courses_list_page.check_visible_courses_title()
     courses_list_page.check_visible_empty_view()
 
